e239fbef174a644f7cf8241b56427dc783f03e75
Jonny Klasson <jonny.klasson@else.se>
2024-11-25 21:20:08 +0100

    D2

--- ConsoleApp/Callendar/D02/Part1.cs
+++ ConsoleApp/Callendar/D02/Part1.cs
@@ -4,8 +4,30 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select(x => x.Split(' '));
+            var horizontal = 0;
+            var depth = 0;
+            foreach (var x in input)
+            {
+                var value = int.Parse(x[1]);
+                switch (x[0])
+                {
+                    case "forward":
+                        horizontal += value;
+                        break;
+                    case "down":
+                        depth += value;
+                        break;
+                    case "up":
+                        depth -= value;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+
+            return (horizontal * depth).ToString();
         }
     }
 }
--- ConsoleApp/Callendar/D02/Part2.cs
+++ ConsoleApp/Callendar/D02/Part2.cs
@@ -4,8 +4,32 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select(x => x.Split(' '));
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+            foreach (var x in input)
+            {
+                var value = int.Parse(x[1]);
+                switch (x[0])
+                {
+                    case "forward":
+                        horizontal += value;
+                        depth += aim * value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+
+            return (horizontal * depth).ToString();
         }
     }
 }
