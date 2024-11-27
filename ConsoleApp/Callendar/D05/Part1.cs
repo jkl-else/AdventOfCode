bbf622a9c03db226cff51eadc7101595c3c5cb5b
Jonny Klasson <jonny.klasson@else.se>
2024-11-27 11:02:51 +0100

    Revisited Day5

--- ConsoleApp/Callendar/D05/Part1.cs
+++ ConsoleApp/Callendar/D05/Part1.cs
@@ -4,51 +4,88 @@ namespace ConsoleApp.Callendar.D05
 {
     internal class Part1 : Part
     {
+        //----------2024----------------------------------------------------------------------------------------------------
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileTextAsync("Input1"); //Result = 389056265 - Result in: 00:00:00.0692409
-            var seeds = new Regex("^seeds: (?<seeds>[0-9 ]*)").Match(input).Groups[1].Value
+            var input = (await ReadFileTextAsync("Input1")).Split(Environment.NewLine + Environment.NewLine);
+            var seeds = input[0]["seeds: ".Length..]
                 .Split(' ')
-                .Select(x => new Seed(long.Parse(x)))
+                .Select(long.Parse)
                 .ToList();
-            var mapgroups = new Regex(@"(?<id>[a-z-]*) map:\r\n(?<map>[0-9 \r\n]*)\r\n")
-                .Matches(input)
-                .Select(x => new
-                {
-                    Id = x.Groups["id"].Value,
-                    Maps = x.Groups["map"].Value
-                    .Split(Environment.NewLine)
-                    .Where(y => y.Length > 0)
-                    .Select(y =>
+            var transformers = input.Skip(1)
+                .Select(x => x.Split(Environment.NewLine).Skip(1) // description row
+                    .Select(s =>
                     {
-                        var values = y.Split(' ').Select(long.Parse).ToArray();
-                        return new Map(values[0], values[1], values[2]);
-                    }).ToList()
-                }).ToList();
-            foreach (var seed in seeds)
+                        var data = s.Split(' ').Select(long.Parse).ToList();
+                        return new Transformer(data[1], data[2], data[0]);
+                    }).ToList());
+            foreach (var transformer in transformers)
             {
-                var throughput = seed.Id;
-                foreach(var mapGroup in mapgroups)
+                for (int i = 0; i < seeds.Count; i++)
                 {
-                    var map = mapGroup.Maps.Find(x => x.ContainsSource(throughput));
-                    if (map == null)
-                        continue;
-                    throughput = map.Evaluate(throughput);
+                    var seed = seeds[i];
+                    seeds[i] = transformer.Where(x => x.CanTransform(seed)).Select(x => x.Transform(seed)).FirstOrDefault(seed);
                 }
-                seed.Location = throughput;
             }
 
-            return seeds.Min(x => x.Location).ToString();
+            return seeds.Min().ToString();
         }
 
-        internal record Map(long Start, long SourceStart, long Range)
+        internal class Transformer(long start, long count, long transformedStart)
         {
-            public bool ContainsSource(long source) => SourceStart <= source && SourceStart + Range >= source;
-            public long Evaluate(long value) => (Start + Range) - (SourceStart + Range - value);
+            public long Start { get; init; } = start;
+            public long End { get; init; } = start + count;
+            public long Diff { get; init; } = transformedStart - start;
+            public bool CanTransform(long value) => value >= Start && value <= End;
+            public long Transform(long value) => value + Diff;
         }
-        internal record Seed(long Id)
-        {
-            public long Location { get; set;}
-        }
+
+        //----------2023----------------------------------------------------------------------------------------------------
+        //public override async Task<string> GetResultAsync()
+        //{
+        //    var input = await ReadFileTextAsync("Input1"); //Result = 389056265 - Result in: 00:00:00.0692409
+        //    var seeds = new Regex("^seeds: (?<seeds>[0-9 ]*)").Match(input).Groups[1].Value
+        //        .Split(' ')
+        //        .Select(x => new Seed(long.Parse(x)))
+        //        .ToList();
+        //    var mapgroups = new Regex(@"(?<id>[a-z-]*) map:\r\n(?<map>[0-9 \r\n]*)\r\n")
+        //        .Matches(input)
+        //        .Select(x => new
+        //        {
+        //            Id = x.Groups["id"].Value,
+        //            Maps = x.Groups["map"].Value
+        //            .Split(Environment.NewLine)
+        //            .Where(y => y.Length > 0)
+        //            .Select(y =>
+        //            {
+        //                var values = y.Split(' ').Select(long.Parse).ToArray();
+        //                return new Map(values[0], values[1], values[2]);
+        //            }).ToList()
+        //        }).ToList();
+        //    foreach (var seed in seeds)
+        //    {
+        //        var throughput = seed.Id;
+        //        foreach(var mapGroup in mapgroups)
+        //        {
+        //            var map = mapGroup.Maps.Find(x => x.ContainsSource(throughput));
+        //            if (map == null)
+        //                continue;
+        //            throughput = map.Evaluate(throughput);
+        //        }
+        //        seed.Location = throughput;
+        //    }
+
+        //    return seeds.Min(x => x.Location).ToString();
+        //}
+
+        //internal record Map(long Start, long SourceStart, long Range)
+        //{
+        //    public bool ContainsSource(long source) => SourceStart <= source && SourceStart + Range >= source;
+        //    public long Evaluate(long value) => (Start + Range) - (SourceStart + Range - value);
+        //}
+        //internal record Seed(long Id)
+        //{
+        //    public long Location { get; set;}
+        //}
     }
 }
--- ConsoleApp/Callendar/D05/Part2.cs
+++ ConsoleApp/Callendar/D05/Part2.cs
@@ -1,103 +1,182 @@
-﻿using System.Text.RegularExpressions;
-
-namespace ConsoleApp.Callendar.D05
+﻿namespace ConsoleApp.Callendar.D05
 {
     internal class Part2 : Part
     {
+        //----------2024----------------------------------------------------------------------------------------------------
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileTextAsync("Input2");//Result = 137516820 - Result in: 00:00:00.0306106
-            var seeds = new Regex("^seeds: (?<seeds>[0-9 ]*)").Match(input).Groups[1].Value
+            var input = (await ReadFileTextAsync("Input2")).Split(Environment.NewLine + Environment.NewLine);
+            var seeds = input[0]["seeds: ".Length..]
                 .Split(' ')
-                .Select((x, i) => new { Value = x, Index = i })
+                .Select((v, i) => new { Value = long.Parse(v), Index = i })
                 .GroupBy(x => x.Index / 2)
-                .Select(x => new SeedCollection(long.Parse(x.First().Value), long.Parse(x.Last().Value)))
+                .Select(x => new Seed(x.First().Value, x.Last().Value - 1))
                 .ToList();
-            List<List<Map>> mapgroups = new Regex(@"(?<id>[a-z-]*) map:\r\n(?<map>[0-9 \r\n]*)\r\n")
-                .Matches(input)
-                .Select(x => x.Groups["map"].Value
-                    .Split(Environment.NewLine)
-                    .Where(y => y.Length > 0)
-                    .Select(y =>
+            var transformers = input.Skip(1)
+                .Select(x => x.Split(Environment.NewLine).Skip(1) // description row
+                    .Select(s =>
                     {
-                        var values = y.Split(' ').Select(long.Parse).ToArray();
-                        return new Map(values[0], values[1], values[2]);
-                    }).ToList()
-                ).ToList();
-            foreach (var seed in seeds)
+                        var data = s.Split(' ').Select(long.Parse).ToArray();
+                        return new Transformer(data[1], data[2], data[0]);
+                    }).ToHashSet());
+
+            foreach (var transformer in transformers)
             {
-                List<ThroughtPut> throughtPuts = new()
+                var transformedSeeds = new List<Seed>();
+                foreach (var seed in seeds)
                 {
-                    new(seed.Id, seed.Range)
-                };
-                foreach (var mapGroup in mapgroups)
-                {
-                    List<ThroughtPut> newThroughputs = new();
-                    foreach (var throughtPut in throughtPuts)
+                    var possibleTransformers = transformer
+                        .Where(x => x.CanTransform(seed))
+                        .OrderBy(x => x.Start)
+                        .ToList();
+                    if (possibleTransformers.Count == 0)
                     {
-                        foreach (var map in mapGroup.Where(x => x.ContainsSource(throughtPut)))
-                        {
-                            var (result, start, end) = map.Evaluate(throughtPut);
-                            throughtPut.Evaluations.Add((start, end));
-                            newThroughputs.Add(result);
-                        }
-                        newThroughputs.AddRange(throughtPut.GetUnevaliated());
+                        transformedSeeds.Add(seed);
+                        continue;
                     }
-                    throughtPuts = newThroughputs;
+
+                    transformedSeeds.AddRange(possibleTransformers
+                        .SelectMany((t, i) => t.Transform(seed, i == 0, i == possibleTransformers.Count - 1)));
                 }
-                seed.Location = throughtPuts.Count == 0
-                    ? long.MaxValue
-                    : throughtPuts.Min(x => x.Start);
+
+                seeds = transformedSeeds;
             }
 
-            return seeds.Min(x => x.Location).ToString(); //511468327 Too High
+            return seeds.Min(x => x.Start).ToString();
         }
 
-        internal record Map(long Start, long SourceStart, long Range)
+        internal class Seed(long start, long count)
         {
-            public bool ContainsSource(ThroughtPut throughtPut)
-                => (throughtPut.Start <= SourceEnd && throughtPut.Start >= SourceStart)
-                || (throughtPut.End >= SourceStart && throughtPut.End <= SourceEnd);
+            public long Start { get; init; } = start;
+            public long Count { get; init; } = count;
+            public long End => Start + Count;
+        }
 
-            public long SourceEnd => SourceStart + Range;
-            public long End => Start + Range;
-            public (ThroughtPut Result, long Start, long End) Evaluate(ThroughtPut value)
+        internal class Transformer(long start, long count, long transformedStart)
+        {
+            public long Start { get; init; } = start;
+            public long End { get; init; } = start + count;
+            public long Diff { get; init; } = transformedStart - start;
+            public bool CanTransform(Seed value) => value.End >= Start && value.Start <= End;
+            public IEnumerable<Seed> Transform(Seed value, bool first, bool last)
             {
-                // get value to start with since all values might not be in this map
-                var valueStartInThisRange = value.Start < SourceStart ? SourceStart : value.Start;
-                // get value to end with since all values might not be in this map
-                var valueEndInThisRange = value.End > SourceEnd ? SourceEnd : value.End;
-                // convert to start + range calulcated by source values
-                var start = valueStartInThisRange + (Start - SourceStart);
-                // convert to end 
-                var end = valueEndInThisRange + (Start - SourceStart);
-                // return new result and which values that this map started and ended with
-                return (new(start, end - start), valueStartInThisRange, valueEndInThisRange);
+                var count = value.Count;
+                var missingStartRange = Start - value.Start;
+                if (missingStartRange > 0)
+                {
+                    count -= missingStartRange;
+                    if (first)
+                        yield return new Seed(value.Start, missingStartRange);
+                }
+
+                var missingEndRange = value.End - End;
+                if (missingEndRange > 0)
+                {
+                    count -= missingEndRange;
+                    if (last)
+                        yield return new Seed(End, missingEndRange);
+                }
+
+                var start = (missingStartRange > 0 ? Start : value.Start) + Diff;
+                yield return new Seed(start, count);
             }
         }
-        internal record SeedCollection(long Id, long Range)
-        {
-            public long Location { get; set; }
-        }
-        public record ThroughtPut(long Start, long Range)
-        {
-            public long End => Start + Range;
-            public List<(long Start, long End)> Evaluations { get; } = new();
-            public IEnumerable<ThroughtPut> GetUnevaliated()
-            {
-                if (Evaluations.Count == 0)
-                {
-                    yield return this; // this hasn't been evaluated yet
-                    yield break;
-                }
-                var currentStart = Start;
-                foreach (var evaluation in Evaluations.OrderBy(x => x.Start))
-                {
-                    if (currentStart < evaluation.Start)
-                        yield return new(currentStart, evaluation.Start - currentStart); // found values that hasn't been evaluated yet
-                    currentStart = evaluation.End;
-                }
-            }
-        }
+
+        //----------2023----------------------------------------------------------------------------------------------------
+        //public override async Task<string> GetResultAsync()
+        //{
+        //    var input = await ReadFileTextAsync("Input2");//Result = 137516820 - Result in: 00:00:00.0306106
+        //    var seeds = new Regex("^seeds: (?<seeds>[0-9 ]*)").Match(input).Groups[1].Value
+        //        .Split(' ')
+        //        .Select((x, i) => new { Value = x, Index = i })
+        //        .GroupBy(x => x.Index / 2)
+        //        .Select(x => new SeedCollection(long.Parse(x.First().Value), long.Parse(x.Last().Value)))
+        //        .ToList();
+        //    List<List<Map>> mapgroups = new Regex(@"(?<id>[a-z-]*) map:\r\n(?<map>[0-9 \r\n]*)\r\n")
+        //        .Matches(input)
+        //        .Select(x => x.Groups["map"].Value
+        //            .Split(Environment.NewLine)
+        //            .Where(y => y.Length > 0)
+        //            .Select(y =>
+        //            {
+        //                var values = y.Split(' ').Select(long.Parse).ToArray();
+        //                return new Map(values[0], values[1], values[2]);
+        //            }).ToList()
+        //        ).ToList();
+        //    foreach (var seed in seeds)
+        //    {
+        //        List<ThroughtPut> throughtPuts = new()
+        //        {
+        //            new(seed.Id, seed.Range)
+        //        };
+        //        foreach (var mapGroup in mapgroups)
+        //        {
+        //            List<ThroughtPut> newThroughputs = new();
+        //            foreach (var throughtPut in throughtPuts)
+        //            {
+        //                foreach (var map in mapGroup.Where(x => x.ContainsSource(throughtPut)))
+        //                {
+        //                    var (result, start, end) = map.Evaluate(throughtPut);
+        //                    throughtPut.Evaluations.Add((start, end));
+        //                    newThroughputs.Add(result);
+        //                }
+        //                newThroughputs.AddRange(throughtPut.GetUnevaliated());
+        //            }
+        //            throughtPuts = newThroughputs;
+        //        }
+        //        seed.Location = throughtPuts.Count == 0
+        //            ? long.MaxValue
+        //            : throughtPuts.Min(x => x.Start);
+        //    }
+
+        //    return seeds.Min(x => x.Location).ToString(); //511468327 Too High
+        //}
+
+        //internal record Map(long Start, long SourceStart, long Range)
+        //{
+        //    public bool ContainsSource(ThroughtPut throughtPut)
+        //        => (throughtPut.Start <= SourceEnd && throughtPut.Start >= SourceStart)
+        //        || (throughtPut.End >= SourceStart && throughtPut.End <= SourceEnd);
+
+        //    public long SourceEnd => SourceStart + Range;
+        //    public long End => Start + Range;
+        //    public (ThroughtPut Result, long Start, long End) Evaluate(ThroughtPut value)
+        //    {
+        //        // get value to start with since all values might not be in this map
+        //        var valueStartInThisRange = value.Start < SourceStart ? SourceStart : value.Start;
+        //        // get value to end with since all values might not be in this map
+        //        var valueEndInThisRange = value.End > SourceEnd ? SourceEnd : value.End;
+        //        // convert to start + range calulcated by source values
+        //        var start = valueStartInThisRange + (Start - SourceStart);
+        //        // convert to end 
+        //        var end = valueEndInThisRange + (Start - SourceStart);
+        //        // return new result and which values that this map started and ended with
+        //        return (new(start, end - start), valueStartInThisRange, valueEndInThisRange);
+        //    }
+        //}
+        //internal record SeedCollection(long Id, long Range)
+        //{
+        //    public long Location { get; set; }
+        //}
+        //public record ThroughtPut(long Start, long Range)
+        //{
+        //    public long End => Start + Range;
+        //    public List<(long Start, long End)> Evaluations { get; } = new();
+        //    public IEnumerable<ThroughtPut> GetUnevaliated()
+        //    {
+        //        if (Evaluations.Count == 0)
+        //        {
+        //            yield return this; // this hasn't been evaluated yet
+        //            yield break;
+        //        }
+        //        var currentStart = Start;
+        //        foreach (var evaluation in Evaluations.OrderBy(x => x.Start))
+        //        {
+        //            if (currentStart < evaluation.Start)
+        //                yield return new(currentStart, evaluation.Start - currentStart); // found values that hasn't been evaluated yet
+        //            currentStart = evaluation.End;
+        //        }
+        //    }
+        //}
     }
 }
