b758bffc40f17a80a682c75c4558b237ed11877e
Jonny Klasson <jonny.klasson@else.se>
2024-12-01 07:39:28 +0100

    Day1

--- ConsoleApp/Calendar/D01/Part1.cs
+++ ConsoleApp/Calendar/D01/Part1.cs
@@ -2,10 +2,22 @@
 {
     internal class Part1 : Part
     {
-        public override async Task<string> GetResultAsync()
+        public override async Task<string> GetResultAsync() // 1506483
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select(x =>
+                {
+                    var v = x.Split("   ");
+                    return (int.Parse(v[0]), int.Parse(v[1]));
+                })
+                .OrderBy(x => x.Item1)
+                .ToArray();
+            var data = input.Select(x => x.Item2)
+                .OrderBy(x => x)
+                .ToArray();
+            return input.Select((x, i) => Math.Abs(data[i] - x.Item1))
+                .Sum()
+                .ToString();
         }
     }
 }
--- ConsoleApp/Calendar/D01/Part2.cs
+++ ConsoleApp/Calendar/D01/Part2.cs
@@ -2,10 +2,19 @@
 {
     internal class Part2 : Part
     {
-        public override async Task<string> GetResultAsync()
+        public override async Task<string> GetResultAsync() // 23126924
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select(x =>
+                {
+                    var v = x.Split("   ");
+                    return (int.Parse(v[0]), int.Parse(v[1]));
+                })
+                .ToList();
+            var counter = input.GroupBy(x => x.Item2)
+                .ToDictionary(x => x.Key, x => x.Count());
+            return input.Sum(x => x.Item1 * counter.GetValueOrDefault(x.Item1, 0))
+                .ToString();
         }
     }
 }
