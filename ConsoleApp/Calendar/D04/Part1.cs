f0c1fcdf43cbb81b8099bc5cffcd79fa53ac4e05
Jonny Klasson <jonny.klasson@else.se>
2024-12-04 07:12:53 +0100

    D4

--- ConsoleApp/Calendar/D04/Part1.cs
+++ ConsoleApp/Calendar/D04/Part1.cs
@@ -2,10 +2,47 @@
 {
     internal class Part1 : Part
     {
-        public override async Task<string> GetResultAsync()
+        public override async Task<string> GetResultAsync() // 2458
         {
-            var input = (await ReadFileLinesAsync("Test"));
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select((x, i) => new { x, i })
+                .ToDictionary(x => x.i,
+                    x => x.x.Select((y, i) => new { y, i }).ToDictionary(y => y.i, y => y.y));
+
+            var result = 0;
+            foreach (var (y, row) in input)
+            {
+                foreach (var x in row.Where(x => x.Value == 'X').Select(x => x.Key))
+                {
+                    var directions = new[]
+                    {
+                        (x + 1, y),
+                        (x - 1, y),
+                        (x, y + 1),
+                        (x, y - 1),
+                        (x + 1, y + 1),
+                        (x + 1, y - 1),
+                        (x - 1, y + 1),
+                        (x - 1, y - 1)
+                    };
+
+                    foreach (var (dx, dy) in directions)
+                    {
+                        if (!input.TryGetValue(dy, out var mr) || !mr.TryGetValue(dx, out var mv) || mv != 'M') continue;
+                        
+                        var ax = dx + (dx - x);
+                        var ay = dy + (dy - y);
+                        if (!input.TryGetValue(ay, out var ar) || !ar.TryGetValue(ax, out var av) || av != 'A') continue;
+
+                        var sx = ax + (ax - dx);
+                        var sy = ay + (ay - dy);
+                        if (!input.TryGetValue(sy, out var sr) || !sr.TryGetValue(sx, out var sv) || sv != 'S') continue;
+                        result++;
+                    }
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
--- ConsoleApp/Calendar/D04/Part2.cs
+++ ConsoleApp/Calendar/D04/Part2.cs
@@ -2,10 +2,44 @@
 {
     internal class Part2 : Part
     {
-        public override async Task<string> GetResultAsync()
+        public override async Task<string> GetResultAsync() // 1945
         {
-            var input = (await ReadFileLinesAsync("Test"));
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select((x, i) => new { x, i })
+                .ToDictionary(x => x.i,
+                    x => x.x.Select((y, i) => new { y, i }).ToDictionary(y => y.i, y => y.y));
+
+            var resultList = new List<(int X, int Y)>();
+
+            foreach (var (y, row) in input)
+            {
+                foreach (var x in row.Where(x => x.Value == 'M').Select(x => x.Key))
+                {
+                    var aPositions = new[]
+                    {
+                        (x + 1, y + 1),
+                        (x + 1, y - 1),
+                        (x - 1, y + 1),
+                        (x - 1, y - 1)
+                    };
+
+                    foreach (var (ax, ay) in aPositions)
+                    {
+                        if (!input.TryGetValue(ay, out var aRow) || !aRow.TryGetValue(ax, out var aValue) || aValue != 'A') continue;
+
+                        var sx = ax + (ax - x);
+                        var sy = ay + (ay - y);
+                        if (!input.TryGetValue(sy, out var sRow) || !sRow.TryGetValue(sx, out var sValue) || sValue != 'S') continue;
+
+                        resultList.Add((ax, ay));
+                    }
+                }
+            }
+
+            return resultList
+                .GroupBy(x => x)
+                .Count(x => x.Count() == 2)
+                .ToString();
         }
     }
 }
