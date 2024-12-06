796368c769c17d3efd595a52d48d7745bcd66eed
Jonny Klasson <jonny.klasson@else.se>
2024-12-06 20:40:44 +0100

    D6

--- ConsoleApp/Calendar/D06/Part1.cs
+++ ConsoleApp/Calendar/D06/Part1.cs
@@ -2,10 +2,44 @@
 {
     internal class Part1 : Part
     {
-        public override async Task<string> GetResultAsync()
+        public override async Task<string> GetResultAsync() // 5131
         {
-            var input = (await ReadFileLinesAsync("Test"));
-            throw new NotImplementedException();
+            var input = (await ReadFileLinesAsync("Input"))
+                .Select((row, yi) => row.Select((c, xi) => new Coordinate(xi, yi, c == '#', c == '^')))
+                .SelectMany(x => x)
+                .ToHashSet();
+            var current = input.First(x => x.Visited);
+            (int X, int Y) direction = new(0, -1);
+            while (true)
+            {
+                var nextStep = new { X = current.X + direction.X, Y = current.Y + direction.Y };
+                var nextPos = input.FirstOrDefault(x => x.X == nextStep.X && x.Y == nextStep.Y);
+                if (nextPos == null) break;
+                if (nextPos.Obstacle)
+                {
+                    direction = direction switch
+                    {
+                        (0, 1) => (-1, 0),
+                        (1, 0) => (0, 1),
+                        (0, -1) => (1, 0),
+                        (-1, 0) => (0, -1),
+                        _ => throw new NotImplementedException()
+                    };
+                }
+                else
+                {
+                    nextPos.Visited = true;
+                    current = nextPos;
+                }
+            }
+            return input.Count(x => x.Visited).ToString();
+        }
+        internal class Coordinate(int x, int y, bool obstacle, bool visited)
+        {
+            public int X { get; } = x;
+            public int Y { get; } = y;
+            public bool Obstacle { get; } = obstacle;
+            public bool Visited { get; set; } = visited;
         }
     }
 }
--- ConsoleApp/Calendar/D06/Part2.cs
+++ ConsoleApp/Calendar/D06/Part2.cs
@@ -2,10 +2,65 @@
 {
     internal class Part2 : Part
     {
-        public override async Task<string> GetResultAsync()
+        public override async Task<string> GetResultAsync() // 1784
         {
-            var input = (await ReadFileLinesAsync("Test"));
-            throw new NotImplementedException();
+            Dictionary<(int X, int Y), (bool Obstacle, bool Start)> input = (await ReadFileLinesAsync("Input"))
+                .Select((row, yi) => row.Select((c, xi) => new { X = xi, Y = yi, Obstacle = c == '#', Guard = c == '^' }))
+                .SelectMany(x => x)
+                .ToDictionary(x => (x.X, x.Y), x => (x.Obstacle, x.Guard));
+
+            var current = input.First(x => x.Value.Start);
+
+            var map = input.Keys.ToHashSet();
+            var obstacles = input.Where(x => x.Value.Obstacle).Select(x => x.Key).ToHashSet();
+            return Patrol(current.Key, map, obstacles, null)
+                .Path
+                .Count(x => x != current.Key && Patrol(current.Key, map, obstacles, x).Loop)
+                .ToString();
+        }
+
+        private static (bool Loop, HashSet<(int X, int Y)> Path) Patrol((int X, int Y) current, ICollection<(int X, int Y)> map, ICollection<(int X, int Y)> obstacles, (int X, int Y)? extraObstacle)
+        {
+            (int X, int Y) direction = new(0, -1);
+            var hits = new Dictionary<(int X, int Y), HashSet<(int X, int Y)>>();
+            HashSet<(int X, int Y)> path = [];
+            var loop = false;
+            while (true)
+            {
+                (int X, int Y) nextStep = (current.X + direction.X, current.Y + direction.Y);
+                if (!map.Contains(nextStep))
+                    break; // outside area
+
+                if (!obstacles.Contains(nextStep) && (!extraObstacle.HasValue || nextStep != extraObstacle.Value))
+                {
+                    // no obstacle
+                    current = nextStep; // move
+                    path.Add(current);
+                    continue;
+                }
+
+                if (!hits.ContainsKey(nextStep))
+                    hits.Add(nextStep, [direction]); // first hit on this obstacle
+                else if (!hits[nextStep].Contains(direction))
+                    hits[nextStep].Add(direction); // second hit but from new direction
+                else
+                {
+                    // already hit this obstacle from this direction
+                    loop = true;
+                    break; // loop
+                }
+                // obstacle hit, change direction
+                direction = direction switch
+                {
+                    (0, 1) => (-1, 0), // down -> left
+                    (1, 0) => (0, 1), // right -> down
+                    (0, -1) => (1, 0), // up -> right
+                    (-1, 0) => (0, -1), // left -> up
+                    _ => throw new NotImplementedException()
+                };
+            }
+
+            return (loop, path); // true = success, false = stuck in loop
         }
     }
 }
